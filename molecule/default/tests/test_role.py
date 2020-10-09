e5087ee6b9025263f866ab77acc3aea04c82e79f
John Freeman <john.freeman.opensource@gmail.com>
2020-10-09 17:05:15 +0100

    Updated Molecule to 3.0.8
    Keeping up with the latest changes.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -1,12 +1,5 @@
 import pytest
 
-import os
-
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
 
 def test_name_set(host):
     name = host.check_output('sudo --set-home --user test_usr1 '
